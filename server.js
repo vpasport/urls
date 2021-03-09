c32e1f6a678bc68b6e9054f43d31626792f8369c
vpasport <pasportnikov2000@gmail.com>
2021-03-09 19:44:47 +0800

    update port

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ server.get(encodeURI('/словарь'), (req, res)=> {
     res.status(301).redirect('https://lifehacker.ru/ispanskij-styd/');
 });
 
-http.createServer(server).listen(3000, () => {
+
+const PORT = process.env.PORT || 5000;
+http.createServer(server).listen(PORT, () => {
     console.log(`Server start`);
 });
